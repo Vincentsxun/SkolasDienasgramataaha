99a8f3ca948310dba0d0ec71f6be9a377fe5b5d3
an <annija.zandare@gmail.com>
2026-03-30 10:01:49 +0300

    .

--- app.py
+++ app.py
@@ -1,18 +1,76 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import json
+import os
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+DATA_FILE = "users.json"
 
-# @app.route("/gallery")
-# def gallery():
-#     return render_template("gallery.html")
 
-# @app.route("/about")
-# def about():
-#     return "<h1>Par šo projektu</h1><p>Šis ir piemērs ar vairākām lapām.</p>"
+def load_users():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return {}
 
-if __name__ == "__main__":
+def save_users(users):
+    with open(DATA_FILE, "w") as f:
+        json.dump(users, f)
+
+users = load_users()
+
+@app.route('/')
+def home():
+    if 'user' in session:
+        return render_template('dashboard.html', user=session['user'])
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    global users
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = request.form['role']
+
+        if username in users:
+            return "User already exists (change username)"
+
+        users[username] = {
+            'password': password,
+            'role': role
+        }
+
+        save_users(users)
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = users.get(username)
+
+        if user and user['password'] == password:
+            session['user'] = {
+                'username': username,
+                'role': user['role']
+            }
+            return redirect(url_for('home'))
+        else:
+            return "Invalid credentials"
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
     app.run(debug=True)
